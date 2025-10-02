cf84f0abb2e2bf7f6a62fa0c7c22b943e81de235
eunjae0621 <chaej@sookmyung.ac.kr>
2025-10-02 15:28:26 +0900

    02-1

--- main.c
+++ main.c
@@ -5,13 +5,14 @@
 
 int main(int argc, char *argv[]) {
 	int num;
-	printf("정수 하나를 입력하시오. : ");
-	scanf("%i", &num);
-	
-	if (num < 0)
-		printf("절대값은 %i 입니다.", -num);
-	else
-	    printf("절대값은 %i 입니다.\n", num);
+   printf("정수 하나를 입력하시오. : ");
+   scanf("%i", &num);
+   
+   if (num < 0)
+      printf("절대값은 %i 입니다.\n", -num);
+   else
+       printf("절대값은 %i 입니다.\n", num);
+
 
 	return 0;
 }
