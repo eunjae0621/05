dbc9320be8ae22680673553d79a86d97d902b57a
eunjae0621 <chaej@sookmyung.ac.kr>
2025-10-02 15:34:04 +0900

    04

--- main.c
+++ main.c
@@ -4,15 +4,16 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int num;
-   printf("정수 하나를 입력하시오. : ");
+   int sum = 0;
+   int num, i;
+   
+   printf("input a number : ");
    scanf("%i", &num);
    
-   if (num < 0)
-      printf("절대값은 %i 입니다.\n", -num);
-   else
-       printf("절대값은 %i 입니다.\n", num);
-
+   for (i=1; i <= num; i++)
+   		sum = sum + i;
+   	
+   	printf("sum is %i\n", sum);
 
 	return 0;
 }
